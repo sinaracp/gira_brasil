b113b5a91987e764e1812dd028d57523051eae1d
carlos Fernandes <carlosrf@gmail.com>
2017-05-14 13:00:51 +0200

    Script PHP pra enviar email

---
+++ mailer.php
@@ -0,0 +1,21 @@
+<?php
+  if(isset($_POST['submit'])) {
+    $to = "info@girabrasil.de";
+    $service_field = $_POST['service'];
+    $name_field = $_POST['name'];
+    $email_field = $_POST['email'];
+    $phone_field = $_POST['phone'];
+    $message = $_POST['message'];
+    $headers = "Content-Type: text/plain; charset=UTF-8";
+
+    $subject = "$name_field / $service_field";
+    $body = "Nome: $name_field\nE-Mail: $email_field\nTelefone: $phone_field \n\nMensagem:\n $message";
+
+
+    mail($to, $subject, $body, $headers);
+    header("Location: http://www.girabrasil.de/pt-br/index-carousel.html");
+  } else {
+    header("Location: http://www.girabrasil.de/pt-br/contato.html");
+  }
+
+?>
